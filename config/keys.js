12225ba7b311dd2b3ae36320f6178bf121e4de41
Andrew MacDonald <40126482+Mac087@users.noreply.github.com>
2020-04-25 11:58:24 -0700

    Delete keys.js

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb://entzarIQ:student123@ds035438.mlab.com:35438/entzariq',
-  secretOrKey: 'secret',
-  AlphaAdv: 'TGDHNBKFQ3CJCP7T',
-  second: '981HFWEVF3KHSHY4'
-};
